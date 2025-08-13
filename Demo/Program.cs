178f56eeb3ac5cc2660edd79b8bd862ab5460750
sohila558 <sohailat.441@gmail.com>
2025-08-13 16:10:48 +0300

    DelegateEX01

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,5 +1,14 @@
-﻿namespace Demo
+﻿using AssignmentADV03;
+
+namespace Demo
 {
+
+    // Step 01 : Create new Datatype (Delegate)
+    public delegate int StringFuncDelegate(string s);
+    // New Delegate (Class): Reference (Pointer) can refer to Function or more (Pointer of Functions)
+    // These Function must have the same Signature of the delegate : int (String)
+    // Regardless Function Name, Parameters Name or Access Modifier
+
     internal class Program
     {
         static void Main(string[] args)
@@ -15,6 +24,26 @@
             // Java : Pure OOP
             // DP  
             #endregion
+
+            #region DelegateEX01
+
+            // Step 02 : Use Delegate
+            //StringFuncDelegate X = StringFunctions.GetCountOfUpperCaseChars;
+
+            //int Count = X.Invoke("Hello World");
+            //Console.WriteLine(Count);
+
+            //StringFuncDelegate X = new StringFuncDelegate(StringFunctions.GetCountOfUpperCaseChars);
+            //StringFuncDelegate X = StringFunctions.GetCountOfUpperCaseChars; // Syntax Sugar
+            //X += StringFunctions.GetCountOfLowerCaseChars;
+
+
+            ////int Count = X.Invoke("Hello World");
+            //int Count = X("Hello World");
+            //Console.WriteLine(Count); 
+            #endregion
+
+
 
 
 
---
+++ Demo/StringFunctions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentADV03
+{
+    internal class StringFunctions
+    {
+        public static int GetCountOfUpperCaseChars(string word)
+        {
+            int count = 0;
+            if (!string.IsNullOrEmpty(word))
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (char.IsUpper(word[i]))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        public static int GetCountOfLowerCaseChars(string word)
+        {
+            int count = 0;
+            if (!string.IsNullOrEmpty(word))
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (char.IsLower(word[i]))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
